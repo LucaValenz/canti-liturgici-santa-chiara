f173295ea98a3fc07c3d38618764fdd55420c3fa
Luca Valenziano <38068415+LucaValenz@users.noreply.github.com>
2026-09-22 12:47:44 +0200

    Add ThemeProvider to App root
    Import ThemeProvider from ./context/ThemeContext and wrap the application providers/router in ThemeProvider in src/App.jsx so the theme context is available app-wide (Navbar, pages, AdminModal). No other behavior changes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import { SongsProvider } from './context/SongsContext'
 import { AdminProvider } from './context/AdminContext'
+import { ThemeProvider } from './context/ThemeContext'
 
 import Navbar from './components/Navbar'
 import AdminModal from './components/AdminModal'
@@ -12,23 +13,25 @@ import './App.css'
 
 export default function App() {
   return (
-    <SongsProvider>
-      <AdminProvider>
-        <Router>
-          <div className="app-layout">
-            <Navbar />
-            <main className="main-content">
-              <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/song/:id" element={<SongPage />} />
-                <Route path="/daily" element={<DailySongsPage />} />
-                <Route path="/suggest" element={<SuggestPage />} />
-              </Routes>
-            </main>
-            <AdminModal />
-          </div>
-        </Router>
-      </AdminProvider>
-    </SongsProvider>
+    <ThemeProvider>
+      <SongsProvider>
+        <AdminProvider>
+          <Router>
+            <div className="app-layout">
+              <Navbar />
+              <main className="main-content">
+                <Routes>
+                  <Route path="/" element={<HomePage />} />
+                  <Route path="/song/:id" element={<SongPage />} />
+                  <Route path="/daily" element={<DailySongsPage />} />
+                  <Route path="/suggest" element={<SuggestPage />} />
+                </Routes>
+              </main>
+              <AdminModal />
+            </div>
+          </Router>
+        </AdminProvider>
+      </SongsProvider>
+    </ThemeProvider>
   )
 }
